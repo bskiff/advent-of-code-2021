c321b3373a74d330bb23d57be9125a0b4cdeb96d
Ben Skiff <benskiff@msn.com>
2021-12-05 03:09:52 -0600

    Add printing of solution to 3

--- 3/src/index.ts
+++ 3/src/index.ts
@@ -1,9 +1,13 @@
-import { computePowerConsumption } from "./diagnostic";
+import {
+  computeLifeSupportRating,
+  computePowerConsumption,
+} from "./diagnostic";
 import { getInput } from "./input-parser";
 
 const solve = async () => {
   const input = await getInput();
   console.log(`part1: ${computePowerConsumption(input)}`);
+  console.log(`part2: ${computeLifeSupportRating(input)}`);
 };
 
 solve();
